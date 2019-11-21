f6a3839cb40161c7481b9e6de651945ab01bd2ee
cgw-9527 <1751866581@qq.com>
2019-11-21 21:28:10 +0800

    modify

--- setup.sh
+++ setup.sh
@@ -23,30 +23,30 @@ command_exists() {
     type "$1" &> /dev/null
 }
 
-for i in "i686" "x86_64"; do
+for i in "i686"; do
     if command_exists wget; then
-        wget --no-check-certificate "$STATHUB_URL/stathub.$i.tar.gz"
+        wget --no-check-certificate "$STATHUB_URL/stathub.$i.tar"
     elif command_exists curl; then
-        curl --insecure -O "$STATHUB_URL/stathub.$i.tar.gz"
+        curl --insecure -O "$STATHUB_URL/stathub.$i.tar"
     else
         echo "Unable to find curl or wget, Please install it and try again."
         exit 1
     fi
 done
 
-if [ ! -f stathub.$(uname -m).tar.gz ]; then
+if [ ! -f stathub.$(uname -m).tar]; then
     echo "Unable to get server file, Please manual download it"
     exit 1
 fi
 
-tar zxf stathub.$(uname -m).tar.gz
+tar zxf stathub.$(uname -m).tar
 chmod +x stathub service
 [ ! -d conf ] && $sudo mkdir $BASEDIR/conf
 if [ ! -f conf/stathub.conf ]; then
     $sudo ./stathub -c conf/stathub.conf --init-server
 fi
 $sudo mkdir $BASEDIR/pkgs
-mv stathub.*.tar.gz $BASEDIR/pkgs
+mv stathub.*.tar $BASEDIR/pkgs
 
 if [ -z "$(grep stathub /etc/rc.local)" ]; then
     $sudo sh -c "echo \"cd $BASEDIR; rm -f log/stathub.pid; ./service start >>$BASEDIR/log/stathub.log 2>&1\" >> /etc/rc.local"
