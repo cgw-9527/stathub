70ec6b59bc3065dd880fc1341ee902490064c581
cgw-9527 <1751866581@qq.com>
2019-11-25 19:52:51 +0800

    omdify

--- src/stat.go
+++ src/stat.go
@@ -59,7 +59,7 @@ type Produce struct {
 	Produceno int `json:"producer:"`
 }
 
-func getMasterNodeList() []MasterNode {
+func getMasterNodeList() []MasterNode { 
 	var masterNode MasterNode
 	var produce Produce
 	var masterNodeList []MasterNode
